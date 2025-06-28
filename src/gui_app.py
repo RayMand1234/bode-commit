0ac37893d7b2c63548931339bd2bd78e347bc7f1
RayMand1234 <raynmgaming@gmail.com>
2025-06-28 15:52:08 +0300

    refactor(few): Change font to heebo

--- src/gui_app.py
+++ src/gui_app.py
@@ -1,9 +1,18 @@
+import os
 import sys
 from PyQt6.QtWidgets import QApplication
 from gui.main_window import ReviewCommitsGUI
+from PyQt6.QtGui import QFontDatabase, QFont
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    # Set heebo font
+    path = os.path.join(os.path.dirname(__file__), './gui/fonts/Heebo-Bold.ttf')
+    font_id = QFontDatabase.addApplicationFont(path)
+    font_family = QFontDatabase.applicationFontFamilies(font_id)[0]
+
+    app.setFont(QFont(font_family))
     gui = ReviewCommitsGUI()
     gui.show()
     sys.exit(app.exec())
